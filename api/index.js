41eb391d61ae2133d681f4f964905ce89d726780
Sandhya Paghdar <sandhyapaghdar@Sandhyas-MacBook-Air.local>
2022-05-21 19:24:10 -0400

    User update API

--- api/index.js
+++ api/index.js
@@ -1,21 +1,25 @@
 const express = require("express");
-const app = express(); 
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
 
 dotenv.config();
 app.use(express.json());
 
 mongoose
-.connect(process.env.Mongo_Url , {
+  .connect(process.env.Mongo_Url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
-}).then(console.log("Connected to Mongoose"))
+
+  }).then(console.log("Connected to Mongoose"))
   .catch((err) => console.log(err));
 
-    app.use("/api/auth" , authRoute)
- app.listen("5001", ()=> {
-     console.log("Port:5001 Backend is running");
- });
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+app.listen("5001", () => {
+  console.log("Port:5001 Backend is running");
+});
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,28 +3,39 @@ const User = require("../models/User");
 const bcrypt = require('bcrypt');
 
 //Update
-router.put("/:id", async (req,res) => {
-    if(req.body.userId === req.params.id){
-        if(req.body.password){
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
             const salt = await bcrypt.genSalt(10);
             req.body.password = await bcrypt.hash(req.body.password, salt);
         }
-    try{
-    } catch(err){
-        res.status(500).json(err);
+        try {
+            const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+                $set: req.body,
+            }, { new: true });
+            res.status(200).json("updated User");
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
-    }
-    else{
+    else {
         res.status(401).json("Only update your account!")
     }
 });
 
 //Delete
-router.put("/:id", async (req,res) => {
-    try{
-
-    } catch(err){
-        res.status(500).json(err);
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        
+        try {
+            await User.findByIdAndDelete(re.params.id)
+            res.status(200).json("User has been deleted");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+    else {
+        res.status(401).json("Only delete your account!")
     }
 });
 
