3c422adfe424297f4c29e790915d86d54ff5e085
Sandhya Paghdar <sandhyapaghdar@Sandhyas-MacBook-Air.local>
2022-05-22 11:32:55 -0400

    Image upload API

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -10,6 +10,17 @@ router.post("/", async(req,res)=> {
     }catch(err){
         res.status(500).json(err);
     }
+});
+
+router.get("/", async(req,res)=> {
+    const newCat = new Category(req.body);
+    try{
+        const cats = await Category.find();
+        res.status(200).json(cats);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
